f3a3ee814e604a1fd7394bbdaeccecad5ad429fb
vaishnavisawant57 <80047521+vaishnavisawant57@users.noreply.github.com>
2021-09-15 14:52:12 +0530

    added a new sentence
    added that this is a good first issue

--- HelloOpenSource.java
+++ HelloOpenSource.java
@@ -1,5 +1,6 @@
 public class HelloOpenSource {
     public static void main(String[] args) {
         System.out.println("Hello Open Source");
+        System.out.println("This is a good first issue");
     }
 }
